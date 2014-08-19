6749e3862cca8311873a6724ac069b0381b900e0
Andre Henry <ahenry@thinkthroughmath.com>
2014-08-18 20:24:23 -0400

    Created Servo controller

--- lib/servo_controller.rb
+++ lib/servo_controller.rb
@@ -1,3 +1,33 @@
+require 'rubygems'
+require 'phidgets-ffi'
+require_relative 'my_logger'
+
 class ServoController
-  # To change this template use File | Settings | File Templates.
+  include Logging
+
+  def initialize
+    @controller = Phidgets::AdvancedServo.new
+    do_attach
+
+    until @controller.attached?
+      logger.info 'Waiting'
+      sleep 10
+    end
+  end
+
+  def do_shutdown
+    @controller.close
+  end
+
+  def do_attach
+    @controller.on_attach  do |device|
+      logger.info "Device attributes: #{device.attributes} attached"
+      logger.info "Class: #{device.device_class}"
+      logger.info "Id: #{device.id}"
+      logger.info "Serial number: #{device.serial_number}"
+      logger.info "Version: #{device.version}"
+      logger.info "# Servos: #{device.advanced_servos.size}"
+    end
+  end
+
 end
