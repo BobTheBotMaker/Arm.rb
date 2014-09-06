c648c0146a99e7fd6877f7fb47e0324b39828700
Andre Henry <ahenry@thinkthroughmath.com>
2014-09-05 20:27:11 -0400

    Encapsulated polling into a wrapper class

---
+++ lib/polling_servo_controller.rb
@@ -0,0 +1,32 @@
+require_relative 'servo_controller'
+
+class PollingServoController
+
+  def initialize(servo_controller)
+    @servo_controller = servo_controller
+  end
+
+  def initialize_servo(port, options)
+    @servo_controller.initialize_servo(port, options)
+  end
+
+  def move_to(servo, position)
+    @servo_controller.move_to(servo, position)
+    until within_tolerance(position, @servo_controller.get_position(servo), 0.01)
+      sleep 1
+    end
+  end
+
+  def disengage_servo(servo)
+    @servo_controller.disengage_servo(servo)
+    while @servo_controller.engaged?(servo)
+      sleep 1
+    end
+  end
+
+  def within_tolerance(required, actual, tolerance)
+    difference = required - actual
+    difference.abs < tolerance
+  end
+
+end
