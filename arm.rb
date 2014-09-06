b883b7ddd2bfb7f3c210065507a00bc364b547a1
Andre Henry <ahenry@thinkthroughmath.com>
2014-09-05 20:26:03 -0400

    Test driver

--- arm.rb
+++ arm.rb
@@ -1,3 +1,10 @@
 require './lib/servo_controller'
+require './lib/polling_servo_controller'
+require './lib/joint'
 
 sc = ServoController.new
+psc = PollingServoController.new(sc)
+jnt = Joint.new(psc, 0, {position_min: 30, position_max: 220, acceleration:90, ramping: true, type: :hitec_hs645mg})
+jnt.move(40)
+jnt.move(180)
+jnt.disengage
