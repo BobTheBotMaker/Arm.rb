143059d53cf9c6f5aa13ab8fa613f76cfed7a4df
Andre Henry <ahenry@thinkthroughmath.com>
2014-09-05 20:25:00 -0400

    Created class to encapsulate joints

--- lib/joint.rb
+++ lib/joint.rb
@@ -1,6 +1,27 @@
+require_relative 'servo_controller'
+
 class Joint
-  def initialize(port, range)
+  include Logging
+
+  def initialize(controller, port, options)
     @port = port
-    @range = range
+    @options = options
+    @controller = controller
+    setup_servo
+    logger.info "Created Joint with Servo Port #{port}"
   end
+
+  def setup_servo
+    @controller.initialize_servo(@port, @options)
+  end
+
+  def move(position)
+    logger.info "Commanded #{@port} to move to position #{position}"
+    @controller.move_to(@port, position)
+  end
+
+  def disengage
+    @controller.disengage_servo(@port)
+  end
+
 end
