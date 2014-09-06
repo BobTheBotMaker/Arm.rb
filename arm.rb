6cb8e83bbe19e1988f2a13ad897b84c4a9170a19
Andre Henry <ahenry@thinkthroughmath.com>
2014-09-05 20:58:59 -0400

    Clean up

--- arm.rb
+++ arm.rb
@@ -1,10 +1,11 @@
 require './lib/servo_controller'
 require './lib/polling_servo_controller'
 require './lib/joint'
+require 'artoo'
 
 sc = ServoController.new
 psc = PollingServoController.new(sc)
-jnt = Joint.new(psc, 0, {position_min: 30, position_max: 220, acceleration:90, ramping: true, type: :hitec_hs645mg})
+jnt = Joint.new(psc, 0, {position_min: 30, position_max: 220, acceleration: 90, ramping: true, type: :hitec_hs645mg})
 jnt.move(40)
 jnt.move(180)
 jnt.disengage
