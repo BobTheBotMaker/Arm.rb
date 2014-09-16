3d747f903f71254e491e04ca84a774ec1daf489c
Andre Henry <ahenry@thinkthroughmath.com>
2014-09-15 20:54:10 -0400

    playing with data mapping

--- arm.rb
+++ arm.rb
@@ -22,22 +22,20 @@ SDL.JoystickEventState(SDL::ENABLE)
 stick = SDL.JoystickOpen(0)
 puts "Name #{SDL.JoystickName(0)}"
 
-#while true
-#
-#  SDL.JoystickUpdate
-#  button = SDL::JoystickGetAxis(stick, 0)
-#  if button != 0
-#    puts button
-#  end
-#
-#end
+def map(x)
+  in_min = -32768.to_f
+  in_max = 32768.to_f
+  out_min = -1.to_f
+  out_max = 1.to_f
+
+  ((x.to_f - in_min) * (out_max - out_min) / (in_max - in_min) + out_min).round(2)
+end
 
 while true
 
-  event = SDL.PollEvent
-   if event != nil
-    puts "Event #{event}"
-  end
+  SDL.JoystickUpdate
+  button = SDL::JoystickGetAxis(stick, 0)
+  puts map(button)
 
 end
 
