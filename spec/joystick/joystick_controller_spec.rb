19f793b6fb640de477f6b1afc441bf0fd9be6770
Andre Henry <ahenry@thinkthroughmath.com>
2014-09-26 16:11:31 -0400

    Update tests

--- spec/joystick/joystick_controller_spec.rb
+++ spec/joystick/joystick_controller_spec.rb
@@ -4,25 +4,36 @@ require 'mocha/mini_test'
 
 describe JoystickController do
   before do
-    @joystick = mock()
-    @receiver = mock()
+    @joystick = mock
+    @receiver = mock
     @joystick_controller = JoystickController::Controller.new(@joystick, JoystickController::PS3_CONTROLLER_MAP)
   end
 
   describe '#update_buttons' do
     it 'should register the select button down' do
       @joystick.stubs(:button).with(0).returns(1).then.with(anything).returns(0)
-      @receiver.expects(:select_button).with(1).once()
+      @receiver.expects(:select_button).with({button: :select, value: 1}).once
       @joystick_controller.on(:select, lambda {|val| @receiver.select_button(val)})
       @joystick_controller.update_buttons
     end
 
     it 'should register the select button up' do
-      @joystick.stubs(:button).with(0).returns(0).then().with(anything).returns(0)
-      @receiver.expects(:select_button).with(0).once()
+      @joystick.stubs(:button).with(0).returns(0).then.with(anything).returns(0)
+      @receiver.expects(:select_button).with({button: :select, value: 0}).once
       @joystick_controller.on(:select, lambda {|val| @receiver.select_button(val)})
       @joystick_controller.update_buttons
     end
 
+  end
+
+  describe '#update_axes' do
+    it 'should register a change on stick 0' do
+      @joystick.stubs(:axis).with(anything).returns(0)
+      @joystick.stubs(:axis).with(0).returns(795)
+      @joystick.stubs(:axis).with(1).returns(895)
+      @receiver.expects(:update_axis).with({stick: :j0, x: 795, y: 895}).once
+      @joystick_controller.on(:j0, lambda {|val| @receiver.update_axis(val)})
+      @joystick_controller.update_axes
+    end
   end
 end
