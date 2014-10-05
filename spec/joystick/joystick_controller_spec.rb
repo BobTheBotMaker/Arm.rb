3443d28623dd3e592f07150e8301cecec89dfcb1
Andre Henry <ahenry@thinkthroughmath.com>
2014-10-05 10:40:19 -0400

    Test stick 1 callbacks

--- spec/joystick/joystick_controller_spec.rb
+++ spec/joystick/joystick_controller_spec.rb
@@ -35,5 +35,15 @@ describe JoystickController do
       @joystick_controller.on(:j0, lambda {|val| @receiver.update_axis(val)})
       @joystick_controller.update_axes
     end
+
+    it 'should register a change on stick 1' do
+      @joystick.stubs(:axis).with(anything).returns(0)
+      @joystick.stubs(:axis).with(0).returns(795)
+      @joystick.stubs(:axis).with(1).returns(895)
+      @receiver.expects(:update_axis).with({stick: :j1, x: 795, y: 895}).once
+      @joystick_controller.on(:j1, lambda {|val| @receiver.update_axis(val)})
+      @joystick_controller.update_axes
+    end
+
   end
 end
