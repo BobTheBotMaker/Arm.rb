6c9094728f7a89de419ed42f8a623e6f49af51bc
Andre Henry <ahenry@thinkthroughmath.com>
2014-10-12 16:05:28 -0400

    Only move if change is larger than 1 and implement incremental move

--- lib/joint/joint.rb
+++ lib/joint/joint.rb
@@ -5,17 +5,31 @@ class Joint
     @port = port
     @options = options
     @controller = controller
+    @position = options[:initial_position]
+
     setup_servo
     logger.info "Created Joint with Servo Port #{port}"
   end
 
   def setup_servo
     @controller.initialize_servo(@port, @options)
+    @current_position = @controller.get_position(@port)
   end
 
   def move(position)
-    logger.info "Commanded #{@port} to move to position #{position}"
-    @controller.move_to(@port, position)
+    delta = (position - @current_position).abs
+    if delta > 1
+      logger.info "Commanded #{@port} to move to position #{position}, current position #{@current_position}"
+      @controller.move(@port, position)
+      @current_position = position
+    end
+  end
+
+  def increment_move(increment)
+    logger.info "Old position #{@position}"
+    @position += increment
+    logger.info "Commanded #{@port} to move #{increment}, new position #{@position}"
+    @controller.move(@port, @position)
   end
 
   def position
